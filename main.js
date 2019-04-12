a17382738a64d3bce4fb77a7a9dfa6be1d464b18
juli3p <julievicart13@gmail.com>
2019-04-12 11:42:59 +0200

    Exercices-3-PARTIE3

---
+++ main.js
@@ -0,0 +1,18 @@
+// Je récupère le premier élèment de la class color qui correspond a '0'
+// Je fais ensuite une fonction pour changer le text en vert
+document.getElementsByClassName('color')[0].addEventListener('click', function(){
+  document.getElementById('text').style.color = 'green';
+}
+);
+// Je récupère le premier élèment de la class color qui correspond a '0'
+// Je fais ensuite une fonction pour changer le text en vert
+document.getElementsByClassName('color')[1].addEventListener('click', function(){
+  document.getElementById('text').style.color = 'red';
+}
+);
+// Je récupère le premier élèment de la class color qui correspond a '0'
+// Je fais ensuite une fonction pour changer le text en vert 
+document.getElementsByClassName('color')[2].addEventListener('click', function(){
+  document.getElementById('text').style.color = 'blue';
+}
+);
